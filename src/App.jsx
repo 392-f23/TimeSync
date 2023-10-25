b7b3e31bad6c6199a6eb04ddda6781397ff80b71
Lucy Beck <lucywbeck@Lucys-Air-7.attlocal.net>
2023-10-25 16:58:48 -0500

    add create event

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EventForm from './components/EventForm';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <EventForm />
+      {/* <h2>Create Event</h2>
+      <form>
+          <label for="eventName">Event Name:</label>
+          <input type="text" id="eventName" name="eventName" required></input>
+
+          <label for="eventDate">Start Date:</label>
+          <input type="date" id="eventDate" name="eventDate" required></input>
+
+          <label for="eventDate">End Date:</label>
+          <input type="date" id="eventDate" name="eventDate" required></input>
+
+          <label for="eventTime">Start Time:</label>
+          <input type="time" id="eventTime" name="eventTime" required></input>
+
+          <label for="eventTime">End Time:</label>
+          <input type="time" id="eventTime" name="eventTime" required></input>
+
+          <input type="submit" value="Create Event"></input>
+      </form> */}
+    
     </div>
   );
 };
---
+++ src/components/EventForm.jsx
@@ -0,0 +1,105 @@
+
+import React, { Component } from 'react';
+import './EventForm.css';
+
+class EventForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventName: '',
+      startDate: '',
+      endDate: '',
+      startTime: '',
+      endTime: '',
+      eventTableData: [],
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleCreateEvent = (event) => {
+    event.preventDefault();
+
+    const { eventName, startDate, endDate, startTime, endTime } = this.state;
+
+    const eventTableData = [];
+    let currentHour = new Date(startDate + ' ' + startTime);
+    const endDateTime = new Date(endDate + ' ' + endTime);
+
+    while (currentHour <= endDateTime) {
+      eventTableData.push({ time: currentHour.toLocaleTimeString(), selected: false });
+      currentHour.setHours(currentHour.getHours() + 1);
+    }
+
+    this.setState({ eventTableData });
+  };
+
+  handleTimeSlotClick = (index) => {
+    const updatedEventTableData = [...this.state.eventTableData];
+    updatedEventTableData[index].selected = !updatedEventTableData[index].selected;
+    this.setState({ eventTableData: updatedEventTableData });
+  };
+
+  render() {
+    const { eventName, startDate, endDate, startTime, endTime, eventTableData } = this.state;
+
+    return (
+      <div>
+        <div>
+        <h2>Create Event</h2>
+        <form onSubmit={this.handleCreateEvent}>
+          <label htmlFor="eventName">Event Name:</label>
+          <input type="text" id="eventName" name="eventName" value={eventName} onChange={this.handleInputChange} required />
+
+          <label htmlFor="startDate">Start Date:</label>
+          <input type="date" id="startDate" name="startDate" value={startDate} onChange={this.handleInputChange} required />
+
+          <label htmlFor="endDate">End Date:</label>
+          <input type="date" id="endDate" name="endDate" value={endDate} onChange={this.handleInputChange} required />
+
+          <label htmlFor="startTime">Start Time:</label>
+          <input type="time" id="startTime" name="startTime" value={startTime} onChange={this.handleInputChange} step="3600" required />
+
+          <label htmlFor="endTime">End Time:</label>
+          <input type="time" id="endTime" name="endTime" value={endTime} onChange={this.handleInputChange} step="3600" required />
+
+          <input type="submit" value="Create Event" />
+        </form>
+      </div>
+    
+        {eventTableData.length > 0 && (
+          <div>
+            <h2>Event Time Slots</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Time</th>
+                  <th>Select</th>
+                </tr>
+              </thead>
+              <tbody>
+                {eventTableData.map((slot, index) => (
+                  <tr key={index}>
+                    <td>{slot.time}</td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        checked={slot.selected}
+                        onChange={() => this.handleTimeSlotClick(index)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default EventForm;
