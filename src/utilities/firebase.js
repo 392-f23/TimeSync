4ab8ff6871f865b523de34fcebc35f0cde164e8f
Lucy Beck <lucywbeck@Lucys-Air-7.attlocal.net>
2023-10-25 16:28:18 -0500

    add firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmX_Jh1LDKNSksQES68l166fPT-Bobtm4",
+  authDomain: "timesync-b19f9.firebaseapp.com",
+  projectId: "timesync-b19f9",
+  storageBucket: "timesync-b19f9.appspot.com",
+  messagingSenderId: "721117239762",
+  appId: "1:721117239762:web:9edb4a8924ec343cff500b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
